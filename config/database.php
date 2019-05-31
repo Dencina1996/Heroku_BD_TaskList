8d8d69ce8518c29283f13ad7ba209ad398c27ffa
David Encina Martínez <davidencina1996@gmail.com>
2019-05-31 14:33:27 +0200

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -16,12 +16,6 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'pgsql'),
-    $url = parse_url(getenv("DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
